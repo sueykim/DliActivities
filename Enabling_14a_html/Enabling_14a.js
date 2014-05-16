8e94ed9ba6f8ae11b6534415c4727521aa268b39
proti9 <proti9@users.noreply.github.com>
2014-05-16 13:50:55 -0700

    Update Enabling_14a.js

--- Enabling_14a_html/Enabling_14a.js
+++ Enabling_14a_html/Enabling_14a.js
@@ -3,39 +3,16 @@ var currentCallAudio;
 //var g_FlashInstalled = false;
 
 $(document).ready(function () {
-	var ua = navigator.userAgent.toLowerCase()
-	if (!g_iPod)
-	  g_iPod = ua.indexOf('iphone') >= 0
-	if (!g_iPod)
-	  g_iPod = ua.indexOf('ipod') >= 0
-	if (!g_iPod)
-	  g_android = ua.indexOf('android') >= 0
-	  
-	var audioTag = document.getElementById("audioPlayer");
-	if(audioTag && audioTag.canPlayType &&
-		audioTag.canPlayType('audio/mpeg')){
-		g_html5_audio = true;
-		//document.title = "Audio found."
-	}
-
-	
-	//g_FlashInstalled = DetectFlashVer(6, 0, 65);
-
-	loadFlashPlayer();
-
-	
-	if(g_html5_audio){
-		loadAudioTagPlayer();
-	}
-
-	loadEmbedPlayer();
-
+	audioInit();
+		  
 	//Default values (for testing)
 	mediaPath = "sampleData/";
-	cssFilename = "styles/Enabling_14a_default.css";
+	cssFilename = "styles/enabling_14a_dlilearn.css";
 	xmlFilename = mediaPath + "MobilePhone.xml";
 	jsonFilename = mediaPath + "MobilePhone.js";
 	
+	$(".activity_hd").html('');
+	$(".activity_description").html('');
 	loadActivity(parseXml);
 });
 
@@ -49,23 +26,64 @@ function parseXml(t_xml)
 	//Load digits
 	var tally = 0;
 	$(xml).find("digit").each(function(){
-		document.getElementById("digit" + tally).innerHTML = $(this).attr("english");
+		$("#num_" + tally + " div").text($(this).attr("english"));
 		tally++;
 	});
 	
+	
+	//Load phoneNumbers
+	$(xml).find("item").each(function(index, v){
+		//Load the answer
+		$(this).append($("<phonenumber id='answer' value='" + 
+							$(this).attr("english")+ "'/>"));
+						
+		//Find at most 9 distractors for the current xml
+		var itemsXml = $(xml).find("item");
+		
+		for(var i=0; i<itemsXml.length; i++){
+			if(i == index)
+				continue;
+			
+			if(i > 9) //A max of nine distractors allowed
+				break;
+			
+			$(this).append($("<phonenumber value='" + 
+						$(itemsXml[i]).attr("english")+ "'/>"));
+		}
+		
+		$(this).find("phonenumber").shuffle();
+	});
+	
+	
+/*	
+	$( "#selectable" ).selectable({
+			selecting: function(event, ui){
+			    if( $(".ui-selected, .ui-selecting").length > 1){
+			    	$(ui.selecting).removeClass("ui-selecting");
+			    }
+		    },
+			selected: function (event, ui) {
+		  		//Find index
+		  		
+			}
+		}
+	);
+*/	
     loadSet(currentSet);
 }
-
 
 function digitPressed(value){
 	var str = document.getElementById('displayText').innerHTML;
 	
-	if(str.length < 15){
+	loadAudioTagPlayer();
+	
+	if(str.length < 9){
 		var digitXml = $(xml).find("digit").eq(value);
 		
-		audio_play( mediaPath + 'mp3/' + digitXml.attr("audio") );
-	
-		document.getElementById('displayText').innerHTML = str + value;
+		audio_play_file(removeFileExt(digitXml.attr("audio")) ,mediaPath);
+			
+		document.getElementById('displayText').innerHTML = str + 
+												digitXml.attr("english");
 	}
 }
 
@@ -78,7 +96,7 @@ function delClicked(){
 }
 
 function playCall(){
-	audio_play(mediaPath + 'mp3/' + currentCallAudio);
+	audio_play_file(removeFileExt(currentCallAudio) ,mediaPath);
 }
 
 var timeoutID;
@@ -88,7 +106,7 @@ var btnLock;
 function callPressed(){
 	var str = document.getElementById('displayText').innerHTML;
 	btnLock = true;
-	
+	//revised for the 14a by SK 20140304
 	if(str == currentPhoneNumber){
 		copyOfText = str;
 		
@@ -97,7 +115,10 @@ function callPressed(){
 		timeoutID = window.setTimeout(correctTimerEnd, 2000);
 		document.getElementById('clickGuard').style.display = "block";
 		
+		
 		$(itemXml).attr('completed', "true");
+		$("#" + currentSet).addClass("itemCompleted");
+		
 		
 		//check for all completed
 		var completed = true;
@@ -178,10 +199,41 @@ var itemXml;
 function loadSet(value){
 	currentSet = value;
 	
+	//setSelected(currentSet);
 	
 	document.getElementById('setText').innerHTML = (currentSet + 1) + "/" + numSets;
 	
 	itemXml = $(xml).find("item").eq(currentSet);
+
+	//Load selection
+	$("#selectable").empty();
+	
+	
+	//Load selections
+	$(itemXml).find("phonenumber").each(function(i, v){
+		var id = false;
+		
+		if($(this).attr("id") == "answer"){
+			id = "answer"	
+		}else{
+			id = "distractor_" + i;
+		}
+		
+		var widthOverride = "";
+		if($(xml).find("content").attr("tl_textfield_width") != undefined){
+			widthOverride = " style='width:" + 
+					$(xml).find("content").attr("tl_textfield_width") +
+					"' ";
+		}
+		
+		var transValue = translatePhoneNum($(v).attr("value"));
+		
+		$("#selectable").append($("<div class='phoneNum noSelect' " + 
+										"id='" + id + "' " + 
+											widthOverride + ">" + 
+										"<div class='phoneNumTxt'>" +
+										 transValue + "</div></div>"));
+	});
 	
 	//Load the scratch text
 	if(itemXml.attr("scratchText")){
@@ -190,27 +242,13 @@ function loadSet(value){
 		document.getElementById("scratchText").value = "Scratch Text Area";
 	}
 	
-	
-	//Grey out the next or prev buttons if necessary
-
-	if(currentSet == 0){
-		document.getElementById("prevBtnClickGuard").style.display = "block";
-	}else{
-		document.getElementById("prevBtnClickGuard").style.display = "none";
-	}
-	
-	if(currentSet == numSets - 1){
-		document.getElementById("nextBtnClickGuard").style.display = "block";
-	}else{
-		document.getElementById("nextBtnClickGuard").style.display = "none";
-	}
-	
 	currentPhoneNumber = $(itemXml).attr('english');
 	currentCallAudio = $(itemXml).attr('audio');
 	
 	loadTlPhoneNum();
 	
 	if($(itemXml).attr('completed') == "true"){
+		$("#answer").addClass('ui-selected');
 		document.getElementById('clickGuard').style.display = "block";
 		document.getElementById('displayText').innerHTML = currentPhoneNumber;
 	}else{
@@ -226,134 +264,24 @@ function loadSet(value){
 }
 
 
-
-var g_html5_audio = false;
-
-var g_android = false;
-var g_iPod = false;
-
-function loadFlashPlayer(){
-	if(!document.getElementById("flashAudioPlayer")){
-		var flashAudioPlayerDiv = document.getElementById("flashAudioPlayerDiv");
-			
-		if(navigator.appName.toLowerCase().indexOf("explorer") != -1){
-			flashAudioPlayerDiv.innerHTML = '<object data="audioPlayer.swf" type="application/x-shockwave-flash" ' +
-		    	'style="width: 0px; height: 0px;" id="flashAudioPlayer">' +
-				    '<param value="audioPlayer.swf" name="movie">' +
-				    '<param value="transparent" name="wmode">' +
-				    '<param name="allowScriptAccess" value="always">' +
-				'</object>';
-		}else{
-			flashAudioPlayerDiv.innerHTML = '<embed align="middle" width="0" height="0" type="application/x-shockwave-flash" ' + 
-				'pluginspage="http://www.adobe.com/go/getflashplayer" allowscriptaccess="sameDomain" ' + 
-				'name="audioPlayer" bgcolor="#869ca7" quality="high" id="flashAudioPlayer" ' + 
-				'src="audioPlayer.swf"></embed>';
-		}
-	}
-}
-
-function loadTlPhoneNum(){
+function translatePhoneNum(phoneNum){
     var out = "";
 
-    for(var i=0; i<currentPhoneNumber.length; i++){
-        var ch = currentPhoneNumber.substr(i,1);
+    for(var i=0; i<phoneNum.length; i++){
+        var ch = phoneNum.substr(i,1);
         var tl_ch = $(xml).find("digit[english='" + ch  + "']");
         out = out + $(tl_ch).text();
     }
 
-    $("#tl_phoneNum").text(out);
+    return out;	
+}
+
+function loadTlPhoneNum(){
+    $("#tl_phoneNum").text(translatePhoneNum(currentPhoneNumber));
 }
 
 
 function loadAudioTagPlayer(){
-	if(!document.getElementById('audioPlayer')){
-		var htmlAudioPlayerDiv = document.getElementById("htmlAudioPlayerDiv");
-		
-		htmlAudioPlayerDiv.innerHTML = '<audio id="audioPlayer" width="0" height="0" autoplay></audio>';
-	}
-}
-
-function loadEmbedPlayer(){
-	if(document.getElementById("id_embed_player")){
-		var embeddedAudioPlayerDiv = document.getElementById("embeddedAudioPlayerDiv");
-		
-		embeddedAudioPlayerDiv.innerHTML = '<embed id="id_embed_player" scale="1" target="myself" type="audio/mpeg" enablejavascript="true" ' + 
-		    'postdomevents="true" showlogo="true" controller="true" bgcolor="gray" ' +
-		    'style="width: 0px; height: 0px;"  autoplay="true"  ></embed> ' ;
-	}
-}
-
-function playFlashAudio(URL){
-	//Android seems to work better with Flash than HTML5
-  	//So use it when available.
-  	
-  	var flashAudioPlayer = document.getElementById("flashAudioPlayer");
-  	
-  	if(flashAudioPlayer && flashAudioPlayer.setAudioFromJS){
-  		flashAudioPlayer.setAudioFromJS(URL);
-  		return true;
-	}else{
-		//something is wrong with the External interface call so fallback by
-		//going to the next in the list
-		return false;
-	}
-}
-
-function playHTMLAudio(URL){
-	document.getElementById('audioPlayer').src = URL;
-	return true; //todo - Right now there is not way to verify that it will be played		
-}
-
-var g_force_player = false;
-
-function audio_play( URL )
-{
-	if(g_force_player){
-		switch(g_force_player){
-			case "flash":
-				playFlashAudio(URL);
-				return;
-			case "html":
-				playHTMLAudio(URL);
-				return;
-		}
-	}
-	
-	if(g_android){
-		if(playFlashAudio(URL)){
-			return;
-		}
-	} 
-
-	if (g_html5_audio){
-		if(playHTMLAudio(URL)){
-			return;
-		}
-	} 
-
-	//Attempt to play using Flash
-	if(playFlashAudio(URL)){
-		return;
-	}
-
-	if (g_android){
-	    window.document.location.href = URL
-	    return;
-	} 
-	
-	//Next attempt to use the embedded player
-	var embedPlayer = document.getElementById("id_embed_player");
-	
-	if (embedPlayer && embedPlayer.Play){
-	    embedPlayer.SetURL(URL)
-	    embedPlayer.Play()
-	}else{
-	    if (window.ActiveXObject){
-	      window.document.location.href = URL
-	      return;
-	    }else{
-	      window.open( URL, '_blank' )
-	      return;
-	    }
-	}
+	var htmlAudioPlayerDiv = document.getElementById("htmlAudioPlayerDiv");
+	htmlAudioPlayerDiv.innerHTML = '<audio id="audioPlayer" width="0" height="0" autoplay></audio>';
 }
