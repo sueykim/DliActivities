9cbf313ace19fee927ef9bb614ea44db6b4b5076
danb-dliflc <daniel.bermender@dliflc.edu>
2014-11-12 10:55:21 -0500

    -Added custom keyboards -Added custom style for Enabling 4 Turkish

---
+++ common/keyboards/french_keyboard.js
@@ -0,0 +1,231 @@
+var jsonKeyboard = {
+    "div": {
+        "__cnt": 78, "div":[ {
+            "__cnt": 3, "_id": "letter1", "_class": "letter roundCornersLetter", "__text": "q"
+        },
+        {
+            "__cnt": 3, "_id": "letter2", "_class": "letter roundCornersLetter", "__text": "w"
+        },
+        {
+            "__cnt": 3, "_id": "letter3", "_class": "letter roundCornersLetter", "__text": "e"
+        },
+        {
+            "__cnt": 3, "_id": "letter4", "_class": "letter roundCornersLetter", "__text": "r"
+        },
+        {
+            "__cnt": 3, "_id": "letter5", "_class": "letter roundCornersLetter", "__text": "t"
+        },
+        {
+            "__cnt": 3, "_id": "letter6", "_class": "letter roundCornersLetter", "__text": "y"
+        },
+        {
+            "__cnt": 3, "_id": "letter7", "_class": "letter roundCornersLetter", "__text": "u"
+        },
+        {
+            "__cnt": 3, "_id": "letter8", "_class": "letter roundCornersLetter", "__text": "i"
+        },
+        {
+            "__cnt": 3, "_id": "letter9", "_class": "letter roundCornersLetter", "__text": "o"
+        },
+        {
+            "__cnt": 3, "_id": "letter10", "_class": "letter roundCornersLetter", "__text": "p"
+        },
+        {
+            "__cnt": 3, "_id": "letter11", "_class": "letter roundCornersLetter", "__text": "a"
+        },
+        {
+            "__cnt": 3, "_id": "letter12", "_class": "letter roundCornersLetter", "__text": "s"
+        },
+        {
+            "__cnt": 3, "_id": "letter13", "_class": "letter roundCornersLetter", "__text": "d"
+        },
+        {
+            "__cnt": 3, "_id": "letter14", "_class": "letter roundCornersLetter", "__text": "f"
+        },
+        {
+            "__cnt": 3, "_id": "letter15", "_class": "letter roundCornersLetter", "__text": "g"
+        },
+        {
+            "__cnt": 3, "_id": "letter16", "_class": "letter roundCornersLetter", "__text": "h"
+        },
+        {
+            "__cnt": 3, "_id": "letter17", "_class": "letter roundCornersLetter", "__text": "j"
+        },
+        {
+            "__cnt": 3, "_id": "letter18", "_class": "letter roundCornersLetter", "__text": "k"
+        },
+        {
+            "__cnt": 3, "_id": "letter19", "_class": "letter roundCornersLetter", "__text": "l"
+        },
+        {
+            "__cnt": 3, "_id": "letter20", "_class": "letter roundCornersLetter", "__text": "z"
+        },
+        {
+            "__cnt": 3, "_id": "letter21", "_class": "letter roundCornersLetter", "__text": "x"
+        },
+        {
+            "__cnt": 3, "_id": "letter22", "_class": "letter roundCornersLetter", "__text": "c"
+        },
+        {
+            "__cnt": 3, "_id": "letter23", "_class": "letter roundCornersLetter", "__text": "v"
+        },
+        {
+            "__cnt": 3, "_id": "letter24", "_class": "letter roundCornersLetter", "__text": "b"
+        },
+        {
+            "__cnt": 3, "_id": "letter25", "_class": "letter roundCornersLetter", "__text": "n"
+        },
+        {
+            "__cnt": 3, "_id": "letter26", "_class": "letter roundCornersLetter", "__text": "m"
+        },
+        {
+            "__cnt": 3, "_id": "letter27", "_class": "letter roundCornersLetter", "__text": "-"
+        },
+        {
+            "__cnt": 3, "_id": "letter28", "_class": "letter roundCornersLetter", "__text": "'"
+        },
+        {
+            "__cnt": 3, "_id": "letter29", "_class": "letter roundCornersLetter", "__text": "ç"
+        },
+        {
+            "__cnt": 3, "_id": "letter30", "_class": "letter roundCornersLetter", "__text": "á"
+        },
+        {
+            "__cnt": 3, "_id": "letter31", "_class": "letter roundCornersLetter", "__text": "à"
+        },
+        {
+            "__cnt": 3, "_id": "letter32", "_class": "letter roundCornersLetter", "__text": "é"
+        },
+        {
+            "__cnt": 3, "_id": "letter33", "_class": "letter roundCornersLetter", "__text": "è"
+        },
+        {
+            "__cnt": 3, "_id": "letter34", "_class": "letter roundCornersLetter", "__text": "ê"
+        },
+        {
+            "__cnt": 3, "_id": "letter35", "_class": "letter roundCornersLetter", "__text": "ù"
+        },
+        {
+            "__cnt": 3, "_id": "letter36", "_class": "letter roundCornersLetter", "__text": "î"
+        },
+        {
+            "__cnt": 3, "_id": "letter37", "_class": "letter roundCornersLetter", "__text": "œ"
+        },
+        {
+            "__cnt": 3, "_id": "letter38", "_class": "letter roundCornersLetter", "__text": "ï"
+        }], "div_asArray":[ {
+            "__cnt": 3, "_id": "letter1", "_class": "letter roundCornersLetter", "__text": "q"
+        },
+        {
+            "__cnt": 3, "_id": "letter2", "_class": "letter roundCornersLetter", "__text": "w"
+        },
+        {
+            "__cnt": 3, "_id": "letter3", "_class": "letter roundCornersLetter", "__text": "e"
+        },
+        {
+            "__cnt": 3, "_id": "letter4", "_class": "letter roundCornersLetter", "__text": "r"
+        },
+        {
+            "__cnt": 3, "_id": "letter5", "_class": "letter roundCornersLetter", "__text": "t"
+        },
+        {
+            "__cnt": 3, "_id": "letter6", "_class": "letter roundCornersLetter", "__text": "y"
+        },
+        {
+            "__cnt": 3, "_id": "letter7", "_class": "letter roundCornersLetter", "__text": "u"
+        },
+        {
+            "__cnt": 3, "_id": "letter8", "_class": "letter roundCornersLetter", "__text": "i"
+        },
+        {
+            "__cnt": 3, "_id": "letter9", "_class": "letter roundCornersLetter", "__text": "o"
+        },
+        {
+            "__cnt": 3, "_id": "letter10", "_class": "letter roundCornersLetter", "__text": "p"
+        },
+        {
+            "__cnt": 3, "_id": "letter11", "_class": "letter roundCornersLetter", "__text": "a"
+        },
+        {
+            "__cnt": 3, "_id": "letter12", "_class": "letter roundCornersLetter", "__text": "s"
+        },
+        {
+            "__cnt": 3, "_id": "letter13", "_class": "letter roundCornersLetter", "__text": "d"
+        },
+        {
+            "__cnt": 3, "_id": "letter14", "_class": "letter roundCornersLetter", "__text": "f"
+        },
+        {
+            "__cnt": 3, "_id": "letter15", "_class": "letter roundCornersLetter", "__text": "g"
+        },
+        {
+            "__cnt": 3, "_id": "letter16", "_class": "letter roundCornersLetter", "__text": "h"
+        },
+        {
+            "__cnt": 3, "_id": "letter17", "_class": "letter roundCornersLetter", "__text": "j"
+        },
+        {
+            "__cnt": 3, "_id": "letter18", "_class": "letter roundCornersLetter", "__text": "k"
+        },
+        {
+            "__cnt": 3, "_id": "letter19", "_class": "letter roundCornersLetter", "__text": "l"
+        },
+        {
+            "__cnt": 3, "_id": "letter20", "_class": "letter roundCornersLetter", "__text": "z"
+        },
+        {
+            "__cnt": 3, "_id": "letter21", "_class": "letter roundCornersLetter", "__text": "x"
+        },
+        {
+            "__cnt": 3, "_id": "letter22", "_class": "letter roundCornersLetter", "__text": "c"
+        },
+        {
+            "__cnt": 3, "_id": "letter23", "_class": "letter roundCornersLetter", "__text": "v"
+        },
+        {
+            "__cnt": 3, "_id": "letter24", "_class": "letter roundCornersLetter", "__text": "b"
+        },
+        {
+            "__cnt": 3, "_id": "letter25", "_class": "letter roundCornersLetter", "__text": "n"
+        },
+        {
+            "__cnt": 3, "_id": "letter26", "_class": "letter roundCornersLetter", "__text": "m"
+        },
+        {
+            "__cnt": 3, "_id": "letter27", "_class": "letter roundCornersLetter", "__text": "-"
+        },
+        {
+            "__cnt": 3, "_id": "letter28", "_class": "letter roundCornersLetter", "__text": "'"
+        },
+        {
+            "__cnt": 3, "_id": "letter29", "_class": "letter roundCornersLetter", "__text": "ç"
+        },
+        {
+            "__cnt": 3, "_id": "letter30", "_class": "letter roundCornersLetter", "__text": "á"
+        },
+        {
+            "__cnt": 3, "_id": "letter31", "_class": "letter roundCornersLetter", "__text": "à"
+        },
+        {
+            "__cnt": 3, "_id": "letter32", "_class": "letter roundCornersLetter", "__text": "é"
+        },
+        {
+            "__cnt": 3, "_id": "letter33", "_class": "letter roundCornersLetter", "__text": "è"
+        },
+        {
+            "__cnt": 3, "_id": "letter34", "_class": "letter roundCornersLetter", "__text": "ê"
+        },
+        {
+            "__cnt": 3, "_id": "letter35", "_class": "letter roundCornersLetter", "__text": "ù"
+        },
+        {
+            "__cnt": 3, "_id": "letter36", "_class": "letter roundCornersLetter", "__text": "î"
+        },
+        {
+            "__cnt": 3, "_id": "letter37", "_class": "letter roundCornersLetter", "__text": "œ"
+        },
+        {
+            "__cnt": 3, "_id": "letter38", "_class": "letter roundCornersLetter", "__text": "ï"
+        }], "_id": "keyboard", "__text":[ "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n\t\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n\t\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n    ", "\n"]
+    }
+}
